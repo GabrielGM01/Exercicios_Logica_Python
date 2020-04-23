02e091d734a185e19f33e716cb9c71c6812cb047
Gabriel Gusmão Moreno <59850618+GabrielGM01@users.noreply.github.com>
2020-04-23 09:43:33 -0300

    Exercicio_6_Logica

---
+++ Conversao_de_Tempo.py
@@ -0,0 +1,17 @@
+"""eia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, 
+   e informe-o expresso no formato horas:minutos:segundos."""
+
+
+
+numero = int(input())
+hora = numero // 60**2
+numero = numero - hora * 60**2
+
+minuto = numero // 60
+numero = numero - minuto*60
+
+segundos = numero
+
+print('{}:{}:{}'.format(hora, minuto, segundos))
+
+
