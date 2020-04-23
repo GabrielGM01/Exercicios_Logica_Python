91499458ab78eae3ad9fbed0f21173cd611e14b9
Gabriel Gusmão Moreno <59850618+GabrielGM01@users.noreply.github.com>
2020-04-23 09:45:58 -0300

    Exercicio_7_Logica

--- Conversao_de_Tempo.py
+++ Conversao_de_Tempo.py
@@ -1,4 +1,4 @@
-"""eia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, 
+"""Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, 
    e informe-o expresso no formato horas:minutos:segundos."""
 
 
