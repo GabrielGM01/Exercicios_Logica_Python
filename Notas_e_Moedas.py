936823c11787b1c640dc9ad2e5e0ce3451d43cd3
Gabriel Gusmão Moreno <59850618+GabrielGM01@users.noreply.github.com>
2020-04-24 23:15:59 -0300

    Exercicio_9_Logica

--- Notas_e_Moedas.py
+++ Notas_e_Moedas.py
@@ -1,3 +1,8 @@
+"""Leia um valor de ponto flutuante com duas casas decimais. 
+   Este valor representa um valor monetário. A seguir, calcule o menor número de notas e moedas possíveis no qual o valor pode ser 
+   decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e 0.01. 
+   A seguir mostre a relação de notas necessárias."""
+
 notas =  float(input())
 
 cem = int(notas/100)
