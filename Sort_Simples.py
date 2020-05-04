bd12a2d8f5955c3d06a97e5a6b8cc5f784556a18
Gabriel Gusmão Moreno <59850618+GabrielGM01@users.noreply.github.com>
2020-05-04 20:24:41 -0300

    Exercicio_15_Logica

--- Sort_Simples.py
+++ Sort_Simples.py
@@ -1,9 +1,14 @@
-inteiros = [1,3,4,5,7,8,9]
-pares = []
-for numero in inteiros:
-    if numero % 2 == 0:
-        pares.append(numero)
+"""Leia 3 valores inteiros e ordene-os em ordem crescente. 
+No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos."""
 
+a,b,c = input().split()
 
-print(pares)
-        
+a = int(a)
+b = int(b)
+c = int(c)
+n1 = [a,b,c]
+n2 = [a,b,c]
+n1.sort(key=int)
+
+print("{}\n{}\n{}\n".format(n1[0],n1[1],n1[2]))
+print("{}\n{}\n{}".format(n2[0],n2[1],n2[2]))
